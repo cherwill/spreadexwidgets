f34bd570b37c1419d7fb85be46f5b6486d30370b
William Chering <wchering@gmail.com>
2024-02-28 15:35:58 +0000

    Initial commit

---
+++ app/SpreadexWidgets/Widgets/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using SpreadexWidgets.Widgets;
+using System.Text;
+
+namespace SpreadexWidgets.Widgets
+{
+    public class Rectangle : Shape
+    {
+        private StringBuilder stringBuilder;
+        private int height;
+        public Rectangle(int xPosition, int yPosition, int width, int height) : base(xPosition, yPosition)
+        {
+            this.Width = width;
+            this.Height = height;
+            stringBuilder = new();
+        }
+
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNegative(value, nameof(value));
+                height = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            stringBuilder.Clear();
+            stringBuilder.AppendFormat("Rectangle ({0},{1}) width={2} height={3}", XPosition, YPosition, Width, Height);
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ app/SpreadexWidgetsTests/Widgets/SquareTests.cs
@@ -0,0 +1,26 @@
+﻿using SpreadexWidgets.Widgets;
+
+namespace SpreadexWidgetsTests.Widgets;
+
+[TestClass]
+public class SquareTests
+{
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void GivenNegativeWidth_ThenThrowsInvalidWidthException()
+    {
+        Square square = new(0, 0, -1);
+    }
+
+    [TestMethod]
+    public void WhenGetDescriptionIsCalled_ThenReturnsValidDescription()
+    {
+        Square square = new(5, 6, 7);
+
+        string expected = "Square (5,6) size=7";
+        string actual = square.ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+}
+
