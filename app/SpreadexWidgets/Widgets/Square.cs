7d21b52e7b3bb0a342e7c6a43c163bd098a22bc4
William Chering <wchering@gmail.com>
2024-02-28 15:38:13 +0000

    Initial commit

---
+++ app/SpreadexWidgets/Widgets/Square.cs
@@ -0,0 +1,22 @@
+﻿using SpreadexWidgets.Widgets;
+using System.Text;
+
+namespace SpreadexWidgets.Widgets
+{
+    public class Square : Shape
+    {
+        private StringBuilder stringBuilder;
+        public Square(int xPosition, int yPosition, int width) : base(xPosition, yPosition, width)
+        {
+            this.Width = width;
+            stringBuilder = new();
+        }
+
+        public override string ToString()
+        {
+            stringBuilder.Clear();
+            stringBuilder.AppendFormat("Square ({0},{1}) size={2}", XPosition, YPosition, Width);
+            return stringBuilder.ToString();
+        }
+    }
+}
