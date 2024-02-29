2a2aad950684f882294ec7051788578cf6f3e83d
William Chering <wchering@gmail.com>
2024-02-29 16:32:14 +0000

    Update Program.cs

--- app/SpreadexWidgets/Program.cs
+++ app/SpreadexWidgets/Program.cs
@@ -33,7 +33,12 @@ namespace SpreadexWidgets
         public static void Main(string[] args)
         {
             IRenderer renderer = new TextRenderer();
-            renderer.DrawSquare(new Square(0, 0, 100));
+
+            renderer.DrawRectangle(new Rectangle(10, 10, 30, 40));
+            renderer.DrawSquare(new Square(15, 30, 35));
+            renderer.DrawEllipse(new Ellipse(100, 150, 300, 200));
+            renderer.DrawCircle(new Circle(1, 1, 300));
+            renderer.DrawTextbox(new Textbox(5, 5, 200, 100, "sample text"));
 
             using Stream stream = new MemoryStream();
             renderer.Render(stream);
@@ -43,8 +48,6 @@ namespace SpreadexWidgets
                 string content = reader.ReadToEnd();
                 Console.WriteLine(content);
             }
-
-            Console.ReadLine();
         }
     }
 }
