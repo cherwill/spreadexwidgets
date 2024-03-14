73583dcc84537fb64c3497e7ca190210a2b5b634
William Chering <wchering@gmail.com>
2024-03-14 17:12:06 +0000

    Add tech test changes

--- SpreadexWidgets.Tests/Widgets/RectangleTests.cs
+++ SpreadexWidgets.Tests/Widgets/RectangleTests.cs
@@ -18,4 +18,45 @@ public class RectangleTests
     {
         Rectangle rectangle = new(0, 0, 0, -1);
     }
+
+    [TestMethod]
+    public void GivenHeightChange_ThenReflectNewHeight()
+    {
+        Rectangle rectangle = new(1, 2, 3, 4);
+
+        var expected = 6;
+
+        rectangle.UpdateHeight(6);
+        Assert.AreEqual(expected, rectangle.Height);
+
+        expected = 7;
+
+        rectangle.UpdateHeight(7);
+        Assert.AreEqual(expected, rectangle.Height);
+
+        expected = 6;
+
+        rectangle.revertHeightChange();
+        Assert.AreEqual(expected, rectangle.Height);
+
+        expected = 4;
+
+        rectangle.revertHeightChange();
+        Assert.AreEqual(expected, rectangle.Height);
+
+
+    }
+
+        [TestMethod]
+    [ExpectedException(typeof(DivideByZeroException))]
+    public void GivenZero_ThenThrowsDivideByZeroException()
+    {
+        calculate(5,0);
+    }
+
+    public int calculate(int a, int b) {
+        return a / b;
+    }
+
+
 }
--- SpreadexWidgets/Widgets/Rectangle.cs
+++ SpreadexWidgets/Widgets/Rectangle.cs
@@ -1,12 +1,20 @@
-﻿namespace SpreadexWidgets.Widgets
+﻿using System.Data;
+
+namespace SpreadexWidgets.Widgets
 {
     public class Rectangle : Widget
     {
+        List<int> widthChanges;
+        List<int> heightChanges;
+
         private int height;
         public Rectangle(int xPosition, int yPosition, int width, int height) : base(xPosition, yPosition)
         {
             this.Width = width;
             this.Height = height;
+            heightChanges = new();
+            widthChanges = new();
+
         }
 
         public int Height
@@ -18,6 +26,53 @@
                 height = value;
             }
 
+        }
+
+        public void UpdateHeight(int newHeight)
+        {
+            if (heightChanges.Count == 0)
+            {
+                heightChanges.Add(Height);
+            }
+            heightChanges.Add(newHeight);
+            Height = newHeight;
+        }
+
+        public void UpdateWidth(int newWidth)
+        {
+            widthChanges.Add(newWidth);
+            Width = heightChanges[heightChanges.Count - 1];
+        }
+
+        public void revertHeightChange()
+        {
+            int length = heightChanges.Count - 1;
+
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length));  
+            }
+            heightChanges.RemoveAt(length);
+
+            length = heightChanges.Count - 1;
+
+            if (length < 0)
+            {
+                throw new IndexOutOfRangeException(nameof(length));
+            }
+
+            Height = heightChanges[length];
+        }
+
+        public void revertWidthChange()
+        {
+            int length = widthChanges.Count - 1;
+
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length));
+            }
+            widthChanges.Remove(length);
         }
     }
 }
